f6a8d2c9d6120d331ab327fc6dd9f08b5afe106e
Vladislav Borovikov <vborovikov@live.ru>
2023-06-30 18:16:30 +0500

    added more tests

--- Pantry.Tests/FractionalTests.cs
+++ Pantry.Tests/FractionalTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Pantry.Tests;
 
 using System;
+using System.Globalization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
@@ -70,5 +71,94 @@ public class FractionalTests
         var fraction = Fractional.Parse("1 1/2");
         Assert.AreEqual("1½", fraction.ToString("V"));
         Assert.AreEqual("1½", fraction.ToString("v"));
+    }
+
+    [TestMethod]
+    public void VulgarFraction_Parsed()
+    {
+        var writing = "⅑";
+        var result = Fractional.TryParse(writing, out var fraction);
+
+        Assert.IsTrue(result);
+        Assert.AreEqual(1, fraction.Numerator);
+        Assert.AreEqual(9, fraction.Denominator);
+    }
+
+    [TestMethod]
+    public void Fraction_ObliqueBarWithInteger_Parsed()
+    {
+        var writing = "5 3/4";
+        var result = Fractional.TryParse(writing, out var fraction);
+
+        Assert.IsTrue(result);
+        Assert.AreEqual(23, fraction.Numerator);
+        Assert.AreEqual(4, fraction.Denominator);
+        Assert.AreEqual("5³⁄₄", fraction.ToString("N"));
+    }
+
+    [TestMethod]
+    public void Fraction_DiagonalBarWithInteger_Parsed()
+    {
+        var writing = "5³⁄₄";
+        var result = Fractional.TryParse(writing, out var fraction);
+
+        Assert.IsTrue(result);
+        Assert.AreEqual(23, fraction.Numerator);
+        Assert.AreEqual(4, fraction.Denominator);
+        Assert.AreEqual("5³⁄₄", fraction.ToString("N"));
+    }
+
+    [TestMethod]
+    public void Fractional_Default_Equality()
+    {
+        var fraction = (Fractional)0.5f;
+
+        EqualityTests.TestEqualObjects(default, new Fractional());
+        EqualityTests.TestUnequalObjects(default, fraction);
+        EqualityTests.TestAgainstNull(fraction);
+    }
+
+    [TestMethod]
+    public void Fractional_TwoValues_NotEqual()
+    {
+        var f1 = (Fractional)1.5f;
+        var f2 = (Fractional)3.15f;
+
+        EqualityTests.TestUnequalObjects(f1, f2);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(FormatException))]
+    public void Fractional_GibberishText_NotParsed()
+    {
+        Assert.IsFalse(Fractional.TryParse("Abracadabra", out var gibberish));
+        Assert.AreEqual(default, gibberish);
+
+        Assert.AreEqual(default, Fractional.Parse("Abracadabra"));
+    }
+
+    [TestMethod]
+    public void Fractional_FractionString_Parsed()
+    {
+        var fraction = (Fractional)0.5;
+        var parsedFraction = Fractional.Parse(fraction.ToString());
+
+        EqualityTests.TestEqualObjects(fraction, parsedFraction);
+    }
+
+    [TestMethod]
+    public void Fractional_DecimalPoint_RealNumber()
+    {
+        var fraction = Fractional.Parse(".5", CultureInfo.InvariantCulture);
+        Assert.AreEqual(0.5f, fraction);
+    }
+
+    [TestMethod]
+    public void Fractional_DashAtEnd_FractionFound()
+    {
+        Assert.IsTrue(Fractional.TryParse("3/4-inch", null, out var fraction, out var length));
+
+        Assert.AreEqual(4, length);
+        Assert.AreEqual(Fractional.Parse("³⁄₄"), fraction);
     }
 }
--- Pantry.Tests/MeasureTests.cs
+++ Pantry.Tests/MeasureTests.cs
@@ -1,6 +1,7 @@
-﻿namespace Pantry.Tests
+namespace Pantry.Tests
 {
     using System;
+    using System.Globalization;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -129,6 +130,122 @@
         {
             var q = Measure.Parse(writing);
             Assert.AreEqual(expected, q.ToString(null, null));
+        }
+
+        [TestMethod]
+        public void Measure_MetricHundredGram_Parsed()
+        {
+            Assert.IsTrue(Measure.TryParse("100 грамм", out var quantity));
+            Assert.AreEqual(100f, quantity.Value.Value);
+            Assert.AreEqual("100 гр", quantity.ToString());
+        }
+
+        [TestMethod]
+        public void Measure_MetricHundredGramDecimalPointRus_Parsed()
+        {
+            Assert.IsTrue(Measure.TryParse("100,00 грамм", CultureInfo.GetCultureInfo("ru-RU"), out var quantity));
+            Assert.AreEqual(100f, quantity.Value.Value);
+            Assert.AreEqual("100 гр", quantity.ToString());
+        }
+
+        [TestMethod]
+        public void Measure_MetricHundredGramDecimalPointEng_Parsed()
+        {
+            Assert.IsTrue(Measure.TryParse("100.00 gram)", CultureInfo.GetCultureInfo("en-US"), out var quantity));
+            Assert.AreEqual(100f, quantity.Value.Value);
+            Assert.AreEqual("100 gr", quantity.ToString());
+        }
+
+        [TestMethod]
+        public void Measure_OneCup_8floz()
+        {
+            Assert.IsTrue(Measure.TryParse("1 cup", out var quantity));
+            Assert.AreEqual(8f, quantity.Value.Value);
+            Assert.AreEqual("1 cup", quantity.ToString());
+        }
+
+        [TestMethod]
+        public void Measure_TwoCups_16floz()
+        {
+            Assert.IsTrue(Measure.TryParse("2 c.", out var quantity));
+            Assert.AreEqual(16f, quantity.Value.Value);
+            Assert.AreEqual("2 cups", quantity.ToString());
+        }
+
+        [TestMethod]
+        public void Measure_OneAndHalfCups_12floz()
+        {
+            Assert.IsTrue(Measure.TryParse("1 1/2 cups", out var quantity));
+            Assert.AreEqual(12f, quantity.Value.Value);
+            Assert.AreEqual("1 1/2 cups", quantity.ToString());
+        }
+
+        [TestMethod]
+        public void Measure_OneFluidOunce_Parsed()
+        {
+            Assert.IsTrue(Measure.TryParse("1 fl. oz.", out var quantity));
+            Assert.AreEqual(1f, quantity.Value.Value);
+            Assert.AreEqual("2 tbsps", quantity.ToString());
+        }
+
+        [TestMethod]
+        public void Measure_OneFluidOunceUnderscore_Parsed()
+        {
+            Assert.IsTrue(Measure.TryParse("1 fl_oz", out var quantity));
+            Assert.AreEqual(1f, quantity.Value.Value);
+            Assert.AreEqual("2 tbsps", quantity.ToString());
+        }
+
+        [TestMethod]
+        public void Measure_Default_Equality()
+        {
+            var quantity = new Measure(1f, MeasureUnit.Number);
+
+            EqualityTests.TestEqualObjects(default, new Measure());
+            EqualityTests.TestUnequalObjects(default, quantity);
+            EqualityTests.TestAgainstNull(quantity);
+        }
+
+        [TestMethod]
+        public void Measure_ThreeValues_NotEqual()
+        {
+            var m1 = new Measure(1f, MeasureUnit.Number); 
+            var m2 = new Measure(2f, MeasureUnit.Number);
+            var m3 = Measure.Parse("2 cups");
+
+            EqualityTests.TestUnequalObjects(m1, m2);
+            EqualityTests.TestUnequalObjects(m1, m3);
+            EqualityTests.TestUnequalObjects(m2, m3);
+        }
+
+        [TestMethod]
+        public void Measure_GibberishText_NotParsed()
+        {
+            Assert.IsFalse(Measure.TryParse("Abracadabra", out var gibberish));
+            Assert.AreEqual(default, gibberish);
+        }
+
+        [TestMethod]
+        public void Measure_Text_Parsed()
+        {
+            var quantity = new Measure(1f, MeasureUnit.Number);
+            var parsedQuantity = Measure.Parse(quantity.ToString());
+
+            EqualityTests.TestEqualObjects(quantity, parsedQuantity);
+        }
+
+        [TestMethod]
+        public void Measure_FractionWithAmpersand_Parsed()
+        {
+            Assert.IsTrue(Measure.TryParse("1 & 1/2 Tablespoon", out var measure));
+            Assert.AreEqual("1 1/2 tbsps", measure.ToString());
+        }
+
+        [TestMethod]
+        public void Measure_FractionWithDash_Parsed()
+        {
+            Assert.IsTrue(Measure.TryParse("2-1/2 cups", out var measure));
+            Assert.AreEqual("2 1/2 cups", measure.ToString());
         }
     }
 }
